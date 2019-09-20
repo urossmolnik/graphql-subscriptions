dbe9fefcd9c465c3756f1e070f14f0a18ed05252
Uros Smolnik <uros.smolnik@gmail.com>
2019-09-20 14:57:51 -0700

    Fix withFilter not to cause Promise memory leaks

--- src/with-filter.ts
+++ src/with-filter.ts
@@ -7,25 +7,23 @@ export const withFilter = (asyncIteratorFn: ResolverFn, filterFn: FilterFn): Res
   return (rootValue: any, args: any, context: any, info: any): AsyncIterator<any> => {
     const asyncIterator = asyncIteratorFn(rootValue, args, context, info);
 
-    const getNextPromise = () => {
-      return asyncIterator
-        .next()
-        .then(payload => {
+    const getNextPromise = async() => {
+      while (true) {
+        try {
+          const payload = await asyncIterator.next();
           if (payload.done === true) {
             return payload;
           }
+          const filterResult = filterFn(payload.value, args, context, info);
+          if (filterResult === true) {
+            return payload;
+          }
+        } catch (err) {
+          // Errors are ignored
+        }
 
-          return Promise.resolve(filterFn(payload.value, args, context, info))
-            .catch(() => false)
-            .then(filterResult => {
-              if (filterResult === true) {
-                return payload;
-              }
-
-              // Skip the current value and wait for the next one
-              return getNextPromise();
-            });
-        });
+        // Skip the current value and wait for the next one
+      }
     };
 
     return {
